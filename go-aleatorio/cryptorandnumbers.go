8af7cbdad1189c4433a15573a4bc8185d389e5eb
hache <hernanlaborde@gmail.com>
2021-03-05 17:07:56 -0500

    Se agrega capacidad de agregar minimos negativos.

--- go-aleatorio/cryptorandnumbers.go
+++ go-aleatorio/cryptorandnumbers.go
@@ -3,17 +3,36 @@ package main
 import (
 	"crypto/rand"
 	"fmt"
+	"math/big"
 )
 
-func main() {
-	c := 10
-	b := make([]byte, c)
-	_, err := rand.Read(b)
+func genRandNum(min, max int64) int64 {
+	//Como rand.Int solo entrega números de entre [0,max) se resta a max
+	dif := big.NewInt(max - min)
+
+	//Int returns a uniform random value in [0, max). It panics if max <= 0.
+	n, err := rand.Int(rand.Reader, dif)
 	if err != nil {
-		fmt.Println("error:", err)
-		return
+		panic(err)
 	}
-	// The slice should now contain random bytes instead of only zeroes.
-	fmt.Println(b)
+
+	//se vuelve a sumar el minimo para neutralizar la resta del inicio.
+	return n.Int64() + min
+}
+
+// Muy Importante = [min, max)
+func main() {
+	var min, max int64
+	var cantidad int
+
+	min = 0
+	max = 101
+	cantidad = 10
+
+	for i := 0; i < cantidad; i++ {
+		num := genRandNum(min, max)
+		fmt.Printf("%v ", num)
+	}
+	fmt.Println("")
 
 }
