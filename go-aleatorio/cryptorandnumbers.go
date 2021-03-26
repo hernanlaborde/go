d4be14d43029d13c245eaacd3320cfad7f633b59
hache <hernanlaborde@gmail.com>
2021-03-25 20:28:21 -0500

    Se agrega generación de archivo csv opcional.

--- go-aleatorio/cryptorandnumbers.go
+++ go-aleatorio/cryptorandnumbers.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"log"
 	"math/big"
+	"os"
+	"strconv"
 )
 
 /*
 Ejemplos de ejecución.
-Genera 10 números aleatorios entre [0,100]
-go run cryptorandnumbers.go -min=0 -max=101 -cant=10
+Genera 1000 números aleatorios entre [1,7) generando archivo csv
+go run cryptorandnumbers.go -cant 1000 -min 1 -max 7 -csv
 Muestra el help:
 go run cryptorandnumbers.go -h
 */
@@ -28,19 +32,45 @@ func genRandNum(min, max int64) int64 {
 	return n.Int64() + min
 }
 
-// Muy Importante = [min, max)
+//Muy Importante = [min, max)
 func main() {
 
 	min := flag.Int64("min", 0, "Valor mínimo (limite minimo incluido [min,max) )")
 	max := flag.Int64("max", 2, "Valor máximo (limite máximo excluido [min,max) )")
 	cantidad := flag.Int("cant", 0, "Cantidad de números aleatorios a generar")
+	csvoutput := flag.Bool("csv", false, "Genera archivo csv con los numeros aleatorios.")
 
 	flag.Parse()
 
-	for i := 0; i < *cantidad; i++ {
-		num := genRandNum(*min, *max)
-		fmt.Printf("%v ", num)
-	}
-	fmt.Println("")
+	numeros := make([]int64, 0, *cantidad)
 
+	// csvoutput == true -> genera archivo csv
+	if *csvoutput {
+		filename := fmt.Sprintf("random_min-%v-max-%v_x%v.csv", *min, *max, *cantidad)
+		file, err := os.Create(filename)
+		if err != nil {
+			message := "Error al crear el archivo csv"
+			log.Fatal(message, err)
+		}
+		defer file.Close()
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+
+		for i := 0; i < *cantidad; i++ {
+			num := genRandNum(*min, *max)
+			// convierte int64 en []string
+			num_s := []string{strconv.FormatInt(num, 10)}
+			numeros = append(numeros, num)
+			if err := writer.Write(num_s); err != nil {
+				log.Fatalln("Error escribiendo en el archivo csv.")
+			}
+		}
+		fmt.Printf("Se genera archivo: %v \n\n", filename)
+	} else {
+		for i := 0; i < *cantidad; i++ {
+			num := genRandNum(*min, *max)
+			numeros = append(numeros, num)
+		}
+	}
+	fmt.Printf("%v \n \n", numeros)
 }
